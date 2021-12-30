7343e09344839c031ac79d2729fbf90924b9e00c
= <=>
2021-12-30 11:33:37 -0500

    se añadio la carpeta traduccionPaises

--- src/public/scripts/tiposDeBusqueda/busquedaPorCapital.js
+++ src/public/scripts/tiposDeBusqueda/busquedaPorCapital.js
@@ -1,7 +1,7 @@
 import plantillaDelPais from "../templates/plantillaDePais.js"
 
 export default function busquedaDePaisPorCapital(nombrePais) {
-    return fetch(`https://restcountries.com/v3.1/name/${nombrePais}`)
+    return fetch(`https://restcountries.com/v3.1/capital/${nombrePais}`)
     .then(res => res.json())
     .then(pais => {
         return plantillaDelPais(pais)
