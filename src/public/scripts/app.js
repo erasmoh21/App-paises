946411d9f84d8337cb37166d52b60fa686d2fa33
= <=>
2022-02-06 15:48:25 -0500

    Se modifico la pagina para que sea responsive

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -1,6 +1,8 @@
 import elementosDelDom from "./utils/elementosDelDom.js"
 import cambioDePlaceholderDeInput from "./utils/cambioPlaceHolderInput.js"
 import manejadorDeTipoDePeticiones from "./utils/manejadorDePeticiones.js"
+import buscarCoincidencias from './busquedaCoincidenciasInput/busquedaCoincidencias.js'
+let paisABuscar = ''
 
 elementosDelDom.formulario.addEventListener('submit',(e) => {
     e.preventDefault()
@@ -15,4 +17,13 @@ elementosDelDom.formulario.addEventListener('submit',(e) => {
 
 elementosDelDom.select.addEventListener('change',() => {
     cambioDePlaceholderDeInput(elementosDelDom.select,elementosDelDom.pais)
+})
+
+elementosDelDom.pais.addEventListener('keypress', (e) => {
+    paisABuscar += e.key 
+    
+    elementosDelDom.contenedorSugerencia.style.display = "block"
+    buscarCoincidencias(paisABuscar).forEach(el => {
+        elementosDelDom.contenedorSugerencia.innerHTML = `<p class="paisesSugeridos">${el}</p>` 
+    })
 })
